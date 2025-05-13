9d90784804ed5f4b1e6216d824fa3618845e1682
lokoajibola <loko.ajibola@gmail.com>
2025-05-13 03:03:38 +0100

    Update: pause before checking carte

--- tests/test_ecommerce.py
+++ tests/test_ecommerce.py
@@ -1,7 +1,8 @@
 import pytest
+import time
 from selenium import webdriver
-# from ..pages.login_page import LoginPage
-from ecommerce_web_tests.pages.login_page import LoginPage  # Assuming your package is named 'ecommerce'
+from pages.login_page import LoginPage
+# from ecommerce_web_tests.pages.login_page import LoginPage  # Assuming your package is named 'ecommerce'
 from pages.inventory_page import InventoryPage
 from selenium.webdriver.common.by import By
 
@@ -23,5 +24,7 @@ def test_ecommerce_flow(driver):
     inventory_page = InventoryPage(driver)
     inventory_page.add_item_to_cart()
     inventory_page.go_to_cart()
+    time.sleep(2)
     assert driver.find_element(By.CLASS_NAME, "cart_quantity").text == "1"
+    # assert driver.find_element(By.XPATH, "//div[contains(@class, 'cart_quantity')]").text == "1"
     print("E-commerce flow test passed!")
