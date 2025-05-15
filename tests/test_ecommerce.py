4fee388dbfa2e026c48c1e7e05cf9d394f70117e
lokoajibola <loko.ajibola@gmail.com>
2025-05-15 17:36:42 +0100

    Add Allure reporting to GitHub Actions

--- tests/test_ecommerce.py
+++ tests/test_ecommerce.py
@@ -1,5 +1,6 @@
 import pytest
 import time
+import allure
 from selenium import webdriver
 from pages.login_page import LoginPage
 # from ecommerce_web_tests.pages.login_page import LoginPage  # Assuming your package is named 'ecommerce'
@@ -28,6 +29,8 @@ def driver():
     yield driver
     driver.quit()
 
+@allure.title("Invalid Login Test")
+@allure.description("Tests login with invalid credentials.")
 def test_invalid_login(driver):
     login_page = LoginPage(driver)
     login_page.login("invalid_user", "wrong_password")
@@ -35,6 +38,9 @@ def test_invalid_login(driver):
     assert "Username and password do not match" in error
     print("Invalid login test passed!")
 
+
+@allure.title("E-Commerce Flow Test")
+@allure.description("Tests login and adding an item to the cart on SauceDemo.")
 def test_ecommerce_flow(driver):
     wait = WebDriverWait(driver, 20)
     # Login
@@ -52,9 +58,9 @@ def test_ecommerce_flow(driver):
     inventory_page.go_to_cart()
     time.sleep(20)
     try:
-        assert wait.until(EC.text_to_be_present_in_element((By.CLASS_NAME, "cart_quantity"), "1"))
+        assert wait.until(EC.text_to_be_present_in_element((By.CLASS_NAME, "cart_quantity"), "2"))
     except AssertionError:
-        driver.save_screenshot("error.png")
-        raise
+        allure.attach(driver.get_screenshot_as_png(), name="cart_error", attachment_type=allure.attachment_type.PNG)
+    raise
     # assert driver.find_element(By.XPATH, "//div[contains(@class, 'cart_quantity')]").text == "1"
     print("E-commerce flow test passed!")
